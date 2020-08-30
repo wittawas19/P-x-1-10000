ca6c730a2025b8bcb51d1f1f7d87fa5cdc93679b
wittawas19@gmail.com <wittawas19@gmail.com>
2020-08-30 18:56:19 +0700

    Add project files.

---
+++ lab 3.2/Source.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int sum = 0 ; 
+	for (int i = 1; i < 10001; i++)
+	{
+		for (int j = 1; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				sum = sum + j;
+			}
+		}
+		if (sum == i)
+		{
+			printf("%d \n", i);
+		}
+		sum = 0;
+	}
+}
