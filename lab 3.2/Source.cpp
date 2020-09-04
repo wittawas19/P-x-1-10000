85c3c8ef0f754d233e8de1f4b8dc52b22e9113af
wittawas19@gmail.com <wittawas19@gmail.com>
2020-09-04 20:39:25 +0700

    change i to x and j to i conclude Nothing change

--- lab 3.2/Source.cpp
+++ lab 3.2/Source.cpp
@@ -3,18 +3,18 @@
 int main()
 {
 	int sum = 0 ; 
-	for (int i = 1; i < 10001; i++)
+	for (int x = 1; x < 10001; x++)
 	{
-		for (int j = 1; j < i; j++)
+		for (int i = 1; i < x; i++)
 		{
-			if (i % j == 0)
+			if (x % i == 0)
 			{
-				sum = sum + j;
+				sum = sum + i;
 			}
 		}
-		if (sum == i)
+		if (sum == x)
 		{
-			printf("%d \n", i);
+			printf("%d \n", x);
 		}
 		sum = 0;
 	}
